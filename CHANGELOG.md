2b40b1177ec04633f7a013489e8ce979d7331203
Bob Olde Hampsink <bob@robuust.digital>
2020-10-28 11:51:22 +0100

    Initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0.0 - 28-10-2020
+### Added
+- Initial release
---
+++ src/gateways/Gateway.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace robuust\coinbase\gateways;
+
+use Craft;
+use craft\commerce\omnipay\base\OffsiteGateway;
+use Omnipay\Coinbase\Gateway as OmnipayGateway;
+use Omnipay\Common\AbstractGateway;
+
+/**
+ * Coinbase gateway.
+ */
+class Gateway extends OffsiteGateway
+{
+    // Properties
+    // =========================================================================
+
+    /**
+     * @var string
+     */
+    public $apiKey;
+
+    /**
+     * @var string
+     */
+    public $secret;
+
+    /**
+     * @var string
+     */
+    public $accountId;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function displayName(): string
+    {
+        return Craft::t('commerce', 'Coinbase');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPaymentTypeOptions(): array
+    {
+        return [
+            'purchase' => Craft::t('commerce', 'Purchase (Authorize and Capture Immediately)'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSettingsHtml()
+    {
+        return Craft::$app->getView()->renderTemplate('commerce-coinbase/gatewaySettings', ['gateway' => $this]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+        $rules[] = ['paymentType', 'compare', 'compareValue' => 'purchase'];
+
+        return $rules;
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createGateway(): AbstractGateway
+    {
+        /** @var OmnipayGateway $gateway */
+        $gateway = static::createOmnipayGateway($this->getGatewayClassName());
+
+        $gateway->setApiKey(Craft::parseEnv($this->apiKey));
+        $gateway->setSecret(Craft::parseEnv($this->secret));
+        $gateway->setAccountId(Craft::parseEnv($this->accountId));
+
+        return $gateway;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getGatewayClassName()
+    {
+        return '\\'.OmnipayGateway::class;
+    }
+}
